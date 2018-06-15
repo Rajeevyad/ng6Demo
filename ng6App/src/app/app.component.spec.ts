5810ede36497cbe9d493dc6144559d1498d19b59
ryadav46 <ryadav46@csc.com>
2018-06-15 09:12:37 +0530

    Added single run test settings

--- ng6App/src/app/app.component.spec.ts
+++ ng6App/src/app/app.component.spec.ts
@@ -10,15 +10,15 @@ describe('AppComponent', () => {
       schemas:[CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the ng6App', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'ng6App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('ng6App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
